06b4bcac8ae5ab354dfc14556751386c1da940b2
niculas1989 <nicolas.maranzano@libero.it>
2022-03-02 14:39:39 +0100

    added author in index

--- index.php
+++ index.php
@@ -26,7 +26,8 @@ class Movie
     }
 }
 
-/* //# Primo film
+/* 
+//# Primo film
 $trama_id_1 = 'Basato su una storia vera, The Wolf of Wall Street segue l’impressionante ascesa e la caduta di Jordan Belfort (interpretato dal tre volte candidato all’Academy Award® Leonardo DiCaprio), il broker di New York che conquista una fortuna incredibile truffando milioni di investitori. Il film segue la folle cavalcata di Belfort, un giovane “nuovo arrivato” a Wall Street che si trasforma via via in un corrotto manipolatore dei mercati e in un cowboy della Borsa. Avendo conquistato rapidamente una ricchezza enorme, Jordan la utilizza per comprarsi un’infinita gamma di afrodisiaci: donne, cocaina, automobili, la moglie supermodella e una vita leggendaria fatta di aspirazioni e acquisti senza limiti. Ma mentre la società di Belfort, la Stratton Oakmont, è sulla cresta dell’onda e sguazza nella gratificazione edonistica più estrema, la SEC e l’FBI tengono d’occhio il suo impero contrassegnato dagli eccessi.';
 
 $wolf_of_wall_street = new Movie(1, 'The Wolf Of Wall Street', '23/01/2014', $trama_id_1);
