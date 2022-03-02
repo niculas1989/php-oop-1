678a6cc1ec1bf2d867a12ed3243042a3755c0a4a
niculas1989 <nicolas.maranzano@libero.it>
2022-03-02 13:29:13 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+//| Scriveremo del codice qui
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- Qui, qualora tutto fosse andato a buon fine, vedrò di stampare qualcosa. -->
+</body>
+
+</html>
